b1a996fd46b91c548d48f2d6e18b4606fdd404c0
patrikk001 <p.korba09@gmail.com>
2026-03-27 13:43:34 +0100

    v2

--- app.py
+++ app.py
@@ -3,35 +3,85 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Dummy databáza
-items = [
-]
-
-
-# Root endpoint
-@app.route("/")
-def home():
-    return jsonify({"message": "Flask backend beží!"})
-
 databaza = {
     "students": [
         {
             "id": 1,
             "name": "Samuel Uhrík",
             "age": 12,
-            "iq": 20
-        },{
+            "iq": 20,
+            "image": "https://picsum.photos/200?random=1"
+        },
+        {
             "id": 2,
             "name": "Tomáš Jurčák",
             "age": 61,
-            "iq": 2
-        },{
+            "iq": 2,
+            "image": "https://picsum.photos/200?random=2"
+        },
+        {
             "id": 3,
             "name": "Marko Mihalička",
             "age": 19,
-            "iq": 67
+            "iq": 67,
+            "image": "https://picsum.photos/200?random=3"
+        },
+        {
+            "id": 4,
+            "name": "Lukáš Vindiš",
+            "age": 25,
+            "iq": 100,
+            "image": "https://picsum.photos/200?random=4"
+        },
+        {
+            "id": 5,
+            "name": "Martin Krajčovič",
+            "age": 30,
+            "iq": 120,
+            "image": "https://picsum.photos/200?random=5"
+        },
+        {
+            "id": 6,
+            "name": "Adam Krajčovič",
+            "age": 12,
+            "iq": 1,
+            "image": "https://picsum.photos/200?random=6"
+        },
+        {
+            "id": 7,
+            "name": "Jožo Kováč",
+            "age": 133,
+            "iq": 35,
+            "image": "https://picsum.photos/200?random=7"
+        },
+        {
+            "id": 8,
+            "name": "Janko Kráľ",
+            "age": 19,
+            "iq": 13,
+            "image": "https://picsum.photos/200?random=8"
+        },
+        {
+            "id": 9,
+            "name": "Laco Strike",
+            "age": 33,
+            "iq": 66,
+            "image": "https://picsum.photos/200?random=9"
+        },
+        {
+            "id": 10,
+            "name": "Jozef Krajčovič",
+            "age": 6,
+            "iq": 9,
+            "image": "https://picsum.photos/200?random=10"
         }
     ]
 }
+
+# Root endpoint
+@app.route("/")
+def home():
+    return jsonify({"message": "Flask backend beží!"})
 
 @app.route("/api")
 def api():
@@ -42,7 +92,7 @@ def find_students(student_id):
     for student in databaza["students"]:
         if student["id"] == student_id:
             return jsonify(student)
-    return jsonify({"error": "Student not found"})  
+    return jsonify({"error": "Student not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
